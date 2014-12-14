0065d0d0243a117a92720576295277a0ecc9109b
allensunbo <allensunbo@gmail.com>
2014-12-14 10:48:41 -0500

    add promise

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,15 @@
-var app = angular.module('myApp',[])
-app.controller('myController', function($scope){
+var app = angular.module('myApp', [])
+app.controller('myController', function($scope, $q) {
     $scope.message = 'angular test!';
+    var defer = $q.defer();
+    var promise = defer.promise;
+    promise.then(function success(data) {
+        console.log(data);
+    }, function() {
+
+    });
+    $scope.doClick = function() {
+        console.info('click');
+        defer.resolve('resolved');
+    }
 });
