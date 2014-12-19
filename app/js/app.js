abf5d1588ad5f8d0a3555573a0a087a4331667bc
allensunbo <allensunbo@gmail.com>
2014-12-19 14:21:18 -0500

    movie

--- app/js/app.js
+++ app/js/app.js
@@ -1,33 +1,38 @@
 var app = angular.module('myApp', [])
 
-app.controller('myController', function($scope, $q, $log, UserService) {
-	$scope.message = 'angular test!';
-	UserService.then(
-		function(data) {
-			$log.info(data);
-			$scope.users = data;
-		},
-		function(reason) {
-			$log.error(reason);
-		}
-	);
+app.controller('myController', function($scope, $q, $log, UserService, $http, $sce) {
+    $scope.message = 'angular test!';
+    UserService.then(
+        function(data) {
+            $log.info(data);
+            $scope.users = data;
+        },
+        function(reason) {
+            $log.error(reason);
+        }
+    );
+
+    $http.get('http://localhost:8080/hello')
+        .success(function(data, status, headers, config) {
+            $scope.movies =  $sce.trustAsHtml(data);
+        });
 });
 
 app.service('UserService', function($q, $timeout) {
-	var defer = $q.defer();
-	$timeout(function() {
-		users = [];
-		users.push({
-			'name': 'john'
-		});
-		users.push({
-			'name': 'smith'
-		});
-		users.push({
-			'name': 'allen'
-		});
-		defer.resolve(users);
-		// defer.reject('error retrieving user info');
-	}, 2000);
-	return defer.promise;
+    var defer = $q.defer();
+    $timeout(function() {
+        users = [];
+        users.push({
+            'name': 'john'
+        });
+        users.push({
+            'name': 'smith'
+        });
+        users.push({
+            'name': 'allen'
+        });
+        defer.resolve(users);
+        // defer.reject('error retrieving user info');
+    }, 2000);
+    return defer.promise;
 });
